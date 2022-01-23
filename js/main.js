fdac5012e1e4f956a26aa177bb2b53b184482aae
Maia Francine Endaya <maia.endaya@gmail.com>
2022-01-23 14:49:29 +0800

    added_more_lights_and_room_decos

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.5, 1000);
 const renderer = new THREE.WebGLRenderer();
 
-scene.background = new THREE.Color(0xDCCFEC);
+scene.background = new THREE.Color(0x574073);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -11,13 +11,24 @@ camera.position.z = 25;
 
 //Initializes the lighting
 //Hemisphere Light
-const hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x7366ff, 1);
+const hemisphereLight = new THREE.HemisphereLight(0xffe8d6, 0x261c4f, 0.3);
 scene.add(hemisphereLight);
+
+//Point Light
+const pointLight = new THREE.PointLight(0xff99f8, 0.5, 100);
+pointLight.position.set(-9, -2, 7);
+scene.add(pointLight);
+
+//Directional Light
+const directionalLight = new THREE.DirectionalLight(0xffe8d6, 1, castShadow = true);
+scene.add(directionalLight);
 
 //Initializes the textures to use for couch and table
 const fabricTexture = new THREE.TextureLoader().load('assets/textures/couch_fabric.jpg');
 const woodTexture = new THREE.TextureLoader().load('assets/textures/wood_texture.jpg');
 const rugPattern = new THREE.TextureLoader().load('assets/textures/rug_pattern.jpg');
+const outsideView = new THREE.TextureLoader().load('assets/textures/sunset_texture.jpg');
+const posterPicture = new THREE.TextureLoader().load('assets/textures/poster_texture.jpg');
 
 //Couch
 const couchMaterial = new THREE.MeshLambertMaterial({ map: fabricTexture });
@@ -35,6 +46,23 @@ scene.add(couchSeat);
 scene.add(couchArmR);
 scene.add(couchArmL);
 scene.add(couchBack);
+
+// Window
+const skyGeometry = new THREE.PlaneGeometry(18, 12);
+const skyMaterial = new THREE.MeshBasicMaterial({ map: outsideView, side: THREE.DoubleSide });
+const sky = new THREE.Mesh(skyGeometry, skyMaterial);
+scene.add(sky);
+
+const windowFrameGeometry = new THREE.PlaneGeometry(18, 1.5);
+const windowFrameMaterial = new THREE.MeshLambertMaterial({ map: woodTexture, side: THREE.DoubleSide });
+const windowFrame1 = new THREE.Mesh(windowFrameGeometry, windowFrameMaterial);
+const windowFrame2 = new THREE.Mesh(windowFrameGeometry, windowFrameMaterial);
+const windowFrame3 = new THREE.Mesh(windowFrameGeometry, windowFrameMaterial);
+const windowFrame4 = new THREE.Mesh(windowFrameGeometry, windowFrameMaterial);
+const windowFrame5 = new THREE.Mesh(windowFrameGeometry, windowFrameMaterial);
+scene.add(windowFrame1);
+scene.add(windowFrame2);
+scene.add(windowFrame3);
 
 //Table
 const tableMaterial = new THREE.MeshPhongMaterial({ map: woodTexture });
@@ -60,6 +88,18 @@ const rugMaterial = new THREE.MeshLambertMaterial({ map: rugPattern, side: THREE
 const rug = new THREE.Mesh(rugGeometry, rugMaterial);
 scene.add(rug);
 
+//Poster
+const posterGeometry = new THREE.PlaneGeometry(13, 19);
+const posterMaterial = new THREE.MeshPhongMaterial({ map: posterPicture, side: THREE.DoubleSide });
+const poster = new THREE.Mesh(posterGeometry, posterMaterial);
+scene.add(poster);
+
+//Weird cube
+const weirdCubeGeometry = new THREE.BoxGeometry(2, 2, 2);
+const weirdCubeMaterial = new THREE.MeshNormalMaterial();
+const weirdCube = new THREE.Mesh(weirdCubeGeometry, weirdCubeMaterial);
+scene.add(weirdCube);
+
 
 function animate() {
   requestAnimationFrame(animate);
@@ -83,7 +123,6 @@ function animate() {
   couchBack.position.z = -6;
 
   //Table assembly
-
   tableTop.position.x = -9;
   tableTop.position.y = -5;
   tableTop.position.z = 7;
@@ -104,12 +143,43 @@ function animate() {
   tableLegL2.position.y = -8.5;
   tableLegL2.position.z = 1.5;
 
+  //Sky and Window
+  sky.position.x = -9;
+  sky.position.y = 12;
+  sky.position.z = -7;
+
+  windowFrame1.position.x = -9;
+  windowFrame1.position.y = 18;
+  windowFrame1.position.z = -7;
+
+  windowFrame2.position.x = -9;
+  windowFrame2.position.y = 12;
+  windowFrame2.position.z = -7;
+
+  windowFrame3.position.x = -9;
+  windowFrame3.position.y = 6;
+  windowFrame3.position.z = -7;
+
+  //Poster
+  poster.position.x = 18;
+  poster.position.y = 9;
+  poster.position.z = -7;
+
   //Rug
   rug.position.x = -11;
   rug.position.y = -11;
   rug.position.z = 3;
 
   rug.rotation.x = 14;
+
+  //Weird Cube
+  weirdCube.position.x = -9;
+  weirdCube.position.y = -2;
+  weirdCube.position.z = 7;
+
+  weirdCube.rotation.x += 0.01;
+  weirdCube.rotation.y += 0.01;
+
   //end
   renderer.render(scene, camera);
 }
